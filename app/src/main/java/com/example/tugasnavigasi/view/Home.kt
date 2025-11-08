86d26bfe740369c46126f8f4c19428c556680320
RizkyPratama05 <rizkypp428@gmail.com>
2025-11-08 14:37:28 +0700

    membuat fun TampilanUtama

--- app/src/main/java/com/example/tugasnavigasi/view/Home.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/Home.kt
@@ -1,2 +1,33 @@
 package com.example.tugasnavigasi.view
 
+import android.widget.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugasnavigasi.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilanUtama(
+    OnMasukBtnClick : () -> Unit
+){
