ad5c824e64e804b6f308c4fb25761802a7e694b4
RizkyPratama05 <rizkypp428@gmail.com>
2025-11-08 14:40:29 +0700

    menghapus import

--- app/src/main/java/com/example/tugasnavigasi/view/Home.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/Home.kt
@@ -14,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
